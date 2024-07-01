831d1cefa82d284ef9c2de37b33b9213a4258d16
CarrioliDante <carriolidante@gmail.com>
2024-07-01 20:06:42 -0300

    [ADD] Added categories page, components, api

---
+++ app/(dashboard)/categories/page.tsx
@@ -0,0 +1,66 @@
+'use client'
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Loader2, Plus } from 'lucide-react'
+import { columns } from './columns'
+import { DataTable } from '@/components/data-table'
+import { Skeleton } from '@/components/ui/skeleton'
+import { useNewCategory } from '@/features/categories/hooks/use-new-category'
+import { useBulkDeleteCategories } from '@/features/categories/api/use-bulk-delete-categories'
+import { useGetCategories } from '@/features/categories/api/use-get-categories'
+
+const CategoriesPage = () => {
+  const newCategory = useNewCategory()
+  const deleteCategories = useBulkDeleteCategories()
+  const categoriesQuery = useGetCategories()
+  const categories = categoriesQuery.data || []
+
+  const isDisabled = categoriesQuery.isLoading || deleteCategories.isPending
+
+  if (categoriesQuery.isLoading) {
+    return (
+      <div className='-mx-auto -mt-24 w-full max-w-screen-2xl pb-10'>
+        <Card className='border-none drop-shadow-sm'>
+          <CardHeader>
+            <Skeleton className='h-8 w-48' />
+          </CardHeader>
+          <CardContent>
+            <div className='flex h-[500px] w-full items-center justify-center'>
+              <Loader2 className='size-6 animate-spin text-slate-300' />
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    )
+  }
+
+  return (
+    <div className='mx-auto -mt-24 w-full max-w-screen-2xl pb-10'>
+      <Card className='border-none drop-shadow-sm'>
+        <CardHeader className='gap-y-2 lg:flex-row lg:items-center lg:justify-between'>
+          <CardTitle className='line-clamp-1 text-xl'>
+            Categories page
+          </CardTitle>
+          <Button onClick={newCategory.onOpen} size='sm'>
+            <Plus className='mr-2 size-4' />
+            Add New
+          </Button>
+        </CardHeader>
+        <CardContent>
+          <DataTable
+            filterKey='email'
+            columns={columns}
+            data={categories}
+            onDelete={(row) => {
+              const ids = row.map((r) => r.original.id)
+              deleteCategories.mutate({ ids })
+            }}
+            disabled={isDisabled}
+          />
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
+
+export default CategoriesPage
---
+++ features/categories/api/use-delete-category.ts
@@ -0,0 +1,33 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { InferRequestType, InferResponseType } from 'hono'
+import { toast } from 'sonner'
+
+import { client } from '@/lib/hono'
+
+type ResponseType = InferResponseType<
+  (typeof client.api.categories)[':id']['$delete']
+>
+
+export const useDeleteCategory = (id?: string) => {
+  const queryClient = useQueryClient()
+
+  const mutation = useMutation<ResponseType, Error>({
+    mutationFn: async () => {
+      const response = await client.api.categories[':id']['$delete']({
+        param: { id },
+      })
+      return await response.json()
+    },
+    onSuccess: () => {
+      toast.success('Category deleted')
+      queryClient.invalidateQueries({ queryKey: ['category', { id }] })
+      queryClient.invalidateQueries({ queryKey: ['categories'] })
+      //TODO invalidar summary y transactions
+    },
+    onError: () => {
+      toast.error('Failed to delete category')
+    },
+  })
+
+  return mutation
+}
